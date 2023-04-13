7fe459b6e8d6b9a734e4dd986b0c334a893d6a6c
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-04-13 20:07:18 +0100

    Add MadLibs game - exercise.

---
+++ 2-variables/exercise.js
@@ -0,0 +1,10 @@
+let pluralNoun = "Roses";
+let pluralNoun2 = "Violets";
+let singularNoun = "I";
+let singularNoun2 = "You";
+let color = "red";
+let color2 = "blue";
+
+console.log(pluralNoun + " are " + color);
+console.log(pluralNoun2 + " are " + color2);
+console.log(singularNoun + " love " + singularNoun2);
--- 2-variables/index.js
+++ 2-variables/index.js
@@ -1,12 +1,16 @@
+// Declaring variables.
 var myName;
 let myAge;
 
+// Initialising variables.
 myName = "Tireni";
 myAge = 12;
 
+// Showing type mutability of variables (shadowing).
 var myHeight = "5ft 5inches";
 myHeight = 5.6;
 
+// Printing variable values.
 console.log("My name is " + myName);
 console.log("My age is " + myAge);
 console.log("My height is " + myHeight);
