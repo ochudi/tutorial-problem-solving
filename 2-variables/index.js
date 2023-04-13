30ad8f90fe545d390386747a88726d04afd009f2
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-04-13 20:00:20 +0100

    Add Variables tutorial.

---
+++ 2-variables/index.js
@@ -0,0 +1,12 @@
+var myName;
+let myAge;
+
+myName = "Tireni";
+myAge = 12;
+
+var myHeight = "5ft 5inches";
+myHeight = 5.6;
+
+console.log("My name is " + myName);
+console.log("My age is " + myAge);
+console.log("My height is " + myHeight);
