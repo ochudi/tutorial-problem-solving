6fc26a56e49720261435a7cb51c6dcbba12bcb54
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-04-19 18:15:32 +0100

    Add operator codes.

---
+++ 3-operators/1-assignment/index.js
@@ -0,0 +1,29 @@
+// Part 1 - Assignment
+var myVariable = 5;
+
+let anotherVariable;
+anotherVariable = 7;
+
+console.log("My Variable is: " + myVariable);
+
+// Part 2 - Aliasing
+var a, b, c;
+c = 2;
+b = c;
+a = b;
+
+console.log("The value of a is: " + a);
+
+// Part 3 - Initialization
+var myVar = 9;
+
+// Part 4 - Declaration
+var myName;
+
+myName = "Tobi";
+
+var firstName, lastName;
+
+firstName = "Adaobi";
+lastName = "Arinzechi";
+console.log("My full Name is: " + firstName + " " + lastName);
---
+++ 3-operators/2-unassigned/index.js
@@ -0,0 +1,11 @@
+var a;
+var b;
+var c;
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
